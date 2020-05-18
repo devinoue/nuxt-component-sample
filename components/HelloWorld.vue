3700326ec9b0018973fc68c9937fdb7fb5f26d3b
Masaharu Inoue <pasteur1822@gmail.com>
2020-05-18 23:17:19 +0900

    feat: init

---
+++ components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ helloWorld }}</h1>
+  </div>
+</template>
+<script lang="ts">
+import { ref } from '@vue/composition-api'
+export default {
+  name: '',
+  setup() {
+    const helloWorld = ref('Hello, world!')
+    return {
+      helloWorld
+    }
+  }
+}
+</script>
+<style lang="scss" scoped></style>
